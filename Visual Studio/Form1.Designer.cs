f1b50f24cf00a324937ce1b5dae761a4d4f69664
Echqo <adam.tokarczyk@student.put.poznan.pl>
2016-04-25 15:48:27 +0200

    Visual Studio WindowsForms

---
+++ Visual Studio/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.top1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bottom1 = new System.Windows.Forms.TextBox();
+            this.right1 = new System.Windows.Forms.TextBox();
+            this.left1 = new System.Windows.Forms.TextBox();
+            this.right2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // top1
+            // 
+            this.top1.AccessibleName = "";
+            this.top1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.top1.Location = new System.Drawing.Point(161, 185);
+            this.top1.Name = "top1";
+            this.top1.Size = new System.Drawing.Size(100, 20);
+            this.top1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // bottom1
+            // 
+            this.bottom1.AccessibleName = "";
+            this.bottom1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.bottom1.Location = new System.Drawing.Point(161, 317);
+            this.bottom1.Name = "bottom1";
+            this.bottom1.Size = new System.Drawing.Size(100, 20);
+            this.bottom1.TabIndex = 2;
+            // 
+            // right1
+            // 
+            this.right1.AccessibleName = "";
+            this.right1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.right1.Location = new System.Drawing.Point(267, 211);
+            this.right1.Multiline = true;
+            this.right1.Name = "right1";
+            this.right1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.right1.Size = new System.Drawing.Size(20, 100);
+            this.right1.TabIndex = 3;
+            // 
+            // left1
+            // 
+            this.left1.AccessibleName = "";
+            this.left1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.left1.Location = new System.Drawing.Point(135, 211);
+            this.left1.Multiline = true;
+            this.left1.Name = "left1";
+            this.left1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.left1.Size = new System.Drawing.Size(20, 100);
+            this.left1.TabIndex = 4;
+            // 
+            // right2
+            // 
+            this.right2.AccessibleName = "";
+            this.right2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.right2.Location = new System.Drawing.Point(293, 211);
+            this.right2.Multiline = true;
+            this.right2.Name = "right2";
+            this.right2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.right2.Size = new System.Drawing.Size(20, 100);
+            this.right2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 555);
+            this.Controls.Add(this.right2);
+            this.Controls.Add(this.left1);
+            this.Controls.Add(this.right1);
+            this.Controls.Add(this.bottom1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.top1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox top1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox bottom1;
+        private System.Windows.Forms.TextBox right1;
+        private System.Windows.Forms.TextBox left1;
+        private System.Windows.Forms.TextBox right2;
+    }
+}
+
---
+++ Visual Studio/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        int wartosc;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.PortName = "COM8";
+
+            //TextBox txt1 = new TextBox();
+            top1.BackColor = Color.Red;
+
+
+
+            serialPort1.BaudRate = 9600;
+            try
+            {
+                if(!serialPort1.IsOpen)
+                {
+                    serialPort1.Open();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+        }
+
+        private void Colors()
+        {
+            if((wartosc>=1000)&&(wartosc<2000))
+            {
+                if((wartosc>=1000)&&(wartosc<1100))
+                {
+                    top1.BackColor = Color.DarkOrchid;
+                }
+                if((wartosc >= 1100) && (wartosc < 1200))
+                {
+                    top1.BackColor = Color.YellowGreen;
+                }
+
+            }
+            if ((wartosc >= 2000) && (wartosc < 3000))
+            {
+                if ((wartosc >= 2000) && (wartosc < 2100))
+                {
+                    bottom1.BackColor = Color.DarkOrchid;
+                }
+                if ((wartosc >= 2100) && (wartosc < 2200))
+                {
+                    bottom1.BackColor = Color.YellowGreen;
+                }
+
+            }
+            if ((wartosc >= 3000) && (wartosc < 4000))
+            {
+                if ((wartosc >= 3000) && (wartosc < 3100))
+                {
+                    right1.BackColor = Color.DarkOrchid;
+                }
+                if ((wartosc >= 3100) && (wartosc < 3200))
+                {
+                    right1.BackColor = Color.YellowGreen;
+                }
+
+            }
+            if ((wartosc >= 4000) && (wartosc < 5000))
+            {
+                if ((wartosc >= 4000) && (wartosc < 4100))
+                {
+                    left1.BackColor = Color.DarkOrchid;
+                }
+                if ((wartosc >= 4100) && (wartosc < 4200))
+                {
+                    left1.BackColor = Color.YellowGreen;
+                }
+
+            }
+
+        }
+
+        private void DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+           // throw new NotImplementedException();
+           try
+            {
+                SerialPort spl = (SerialPort)sender;
+
+                string dane = spl.ReadLine();
+                wartosc = Convert.ToInt32(dane);
+                Console.Write("wartosc: " + wartosc + "\n");
+
+                Colors();
+
+            }
+            catch(Exception ex)
+            {
+         
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
