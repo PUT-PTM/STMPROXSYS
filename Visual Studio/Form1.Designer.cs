33e72456c75c524fd103ebced411bb618fbc92ce
Echqo <adam.tokarczyk@student.put.poznan.pl>
2016-05-15 12:06:45 +0200

    Zmiana zasady działania projektu
    Mapowanie zasięgu czujnika zamiast wyświetlania danych odległościowych

--- Visual Studio/Form1.Designer.cs
+++ Visual Studio/Form1.Designer.cs
@@ -30,85 +30,211 @@
         {
             this.components = new System.ComponentModel.Container();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.top1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.bottom1 = new System.Windows.Forms.TextBox();
-            this.right1 = new System.Windows.Forms.TextBox();
-            this.left1 = new System.Windows.Forms.TextBox();
-            this.right2 = new System.Windows.Forms.TextBox();
+            this.buttonON = new System.Windows.Forms.Button();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.text_map = new System.Windows.Forms.TextBox();
+            this.button_W = new System.Windows.Forms.Button();
+            this.button_A = new System.Windows.Forms.Button();
+            this.button_D = new System.Windows.Forms.Button();
+            this.button_S = new System.Windows.Forms.Button();
+            this.text_X_value = new System.Windows.Forms.TextBox();
+            this.text_Y_value = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_SAVE = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // top1
+            // buttonON
             // 
-            this.top1.AccessibleName = "";
-            this.top1.BackColor = System.Drawing.SystemColors.InfoText;
-            this.top1.Location = new System.Drawing.Point(161, 185);
-            this.top1.Name = "top1";
-            this.top1.Size = new System.Drawing.Size(100, 20);
-            this.top1.TabIndex = 0;
+            this.buttonON.BackColor = System.Drawing.Color.DodgerBlue;
+            this.buttonON.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonON.Location = new System.Drawing.Point(387, 439);
+            this.buttonON.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonON.Name = "buttonON";
+            this.buttonON.Size = new System.Drawing.Size(149, 106);
+            this.buttonON.TabIndex = 1;
+            this.buttonON.Text = "MAP";
+            this.buttonON.UseVisualStyleBackColor = false;
+            this.buttonON.Click += new System.EventHandler(this.buttonON_Click);
             // 
-            // button1
+            // labelX
             // 
-            this.button1.Location = new System.Drawing.Point(161, 211);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 100);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelX.Location = new System.Drawing.Point(23, 20);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(21, 19);
+            this.labelX.TabIndex = 3;
+            this.labelX.Text = "X:";
             // 
-            // bottom1
+            // labelY
             // 
-            this.bottom1.AccessibleName = "";
-            this.bottom1.BackColor = System.Drawing.SystemColors.InfoText;
-            this.bottom1.Location = new System.Drawing.Point(161, 317);
-            this.bottom1.Name = "bottom1";
-            this.bottom1.Size = new System.Drawing.Size(100, 20);
-            this.bottom1.TabIndex = 2;
+            this.labelY.AutoSize = true;
+            this.labelY.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelY.Location = new System.Drawing.Point(24, 53);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(20, 19);
+            this.labelY.TabIndex = 4;
+            this.labelY.Text = "Y:";
             // 
-            // right1
+            // text_map
             // 
-            this.right1.AccessibleName = "";
-            this.right1.BackColor = System.Drawing.SystemColors.InfoText;
-            this.right1.Location = new System.Drawing.Point(267, 211);
-            this.right1.Multiline = true;
-            this.right1.Name = "right1";
-            this.right1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.right1.Size = new System.Drawing.Size(20, 100);
-            this.right1.TabIndex = 3;
+            this.text_map.BackColor = System.Drawing.Color.White;
+            this.text_map.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.text_map.ForeColor = System.Drawing.Color.Black;
+            this.text_map.Location = new System.Drawing.Point(68, 109);
+            this.text_map.Multiline = true;
+            this.text_map.Name = "text_map";
+            this.text_map.ReadOnly = true;
+            this.text_map.Size = new System.Drawing.Size(712, 304);
+            this.text_map.TabIndex = 6;
             // 
-            // left1
+            // button_W
             // 
-            this.left1.AccessibleName = "";
-            this.left1.BackColor = System.Drawing.SystemColors.InfoText;
-            this.left1.Location = new System.Drawing.Point(135, 211);
-            this.left1.Multiline = true;
-            this.left1.Name = "left1";
-            this.left1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.left1.Size = new System.Drawing.Size(20, 100);
-            this.left1.TabIndex = 4;
+            this.button_W.BackColor = System.Drawing.Color.Black;
+            this.button_W.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_W.ForeColor = System.Drawing.Color.White;
+            this.button_W.Location = new System.Drawing.Point(611, 439);
+            this.button_W.Name = "button_W";
+            this.button_W.Size = new System.Drawing.Size(40, 40);
+            this.button_W.TabIndex = 7;
+            this.button_W.Text = "W";
+            this.button_W.UseVisualStyleBackColor = false;
+            this.button_W.Click += new System.EventHandler(this.button_W_Click);
             // 
-            // right2
+            // button_A
             // 
-            this.right2.AccessibleName = "";
-            this.right2.BackColor = System.Drawing.SystemColors.InfoText;
-            this.right2.Location = new System.Drawing.Point(293, 211);
-            this.right2.Multiline = true;
-            this.right2.Name = "right2";
-            this.right2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.right2.Size = new System.Drawing.Size(20, 100);
-            this.right2.TabIndex = 5;
+            this.button_A.BackColor = System.Drawing.Color.Black;
+            this.button_A.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_A.ForeColor = System.Drawing.Color.White;
+            this.button_A.Location = new System.Drawing.Point(567, 474);
+            this.button_A.Margin = new System.Windows.Forms.Padding(3, 3, 1, 3);
+            this.button_A.Name = "button_A";
+            this.button_A.Size = new System.Drawing.Size(40, 40);
+            this.button_A.TabIndex = 8;
+            this.button_A.Text = "A";
+            this.button_A.UseVisualStyleBackColor = false;
+            this.button_A.Click += new System.EventHandler(this.button_A_Click);
+            // 
+            // button_D
+            // 
+            this.button_D.BackColor = System.Drawing.Color.Black;
+            this.button_D.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_D.ForeColor = System.Drawing.Color.White;
+            this.button_D.Location = new System.Drawing.Point(655, 474);
+            this.button_D.Margin = new System.Windows.Forms.Padding(1);
+            this.button_D.Name = "button_D";
+            this.button_D.Size = new System.Drawing.Size(40, 40);
+            this.button_D.TabIndex = 9;
+            this.button_D.Text = "D";
+            this.button_D.UseVisualStyleBackColor = false;
+            this.button_D.Click += new System.EventHandler(this.button_D_Click);
+            // 
+            // button_S
+            // 
+            this.button_S.BackColor = System.Drawing.Color.Black;
+            this.button_S.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_S.ForeColor = System.Drawing.Color.White;
+            this.button_S.Location = new System.Drawing.Point(611, 507);
+            this.button_S.Name = "button_S";
+            this.button_S.Size = new System.Drawing.Size(40, 40);
+            this.button_S.TabIndex = 10;
+            this.button_S.Text = "S";
+            this.button_S.UseVisualStyleBackColor = false;
+            this.button_S.Click += new System.EventHandler(this.button_S_Click);
+            // 
+            // text_X_value
+            // 
+            this.text_X_value.BackColor = System.Drawing.Color.White;
+            this.text_X_value.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.text_X_value.Location = new System.Drawing.Point(50, 17);
+            this.text_X_value.Name = "text_X_value";
+            this.text_X_value.ReadOnly = true;
+            this.text_X_value.Size = new System.Drawing.Size(57, 27);
+            this.text_X_value.TabIndex = 11;
+            // 
+            // text_Y_value
+            // 
+            this.text_Y_value.BackColor = System.Drawing.Color.White;
+            this.text_Y_value.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.text_Y_value.Location = new System.Drawing.Point(50, 50);
+            this.text_Y_value.Name = "text_Y_value";
+            this.text_Y_value.ReadOnly = true;
+            this.text_Y_value.Size = new System.Drawing.Size(57, 27);
+            this.text_Y_value.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Y: 200 cm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "X: 0 cm";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(738, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "200 cm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 400);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "20 cm";
+            // 
+            // button_SAVE
+            // 
+            this.button_SAVE.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button_SAVE.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_SAVE.Location = new System.Drawing.Point(144, 441);
+            this.button_SAVE.Name = "button_SAVE";
+            this.button_SAVE.Size = new System.Drawing.Size(149, 106);
+            this.button_SAVE.TabIndex = 17;
+            this.button_SAVE.Text = "SAVE";
+            this.button_SAVE.UseVisualStyleBackColor = false;
+            this.button_SAVE.Click += new System.EventHandler(this.button_SAVE_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 555);
-            this.Controls.Add(this.right2);
-            this.Controls.Add(this.left1);
-            this.Controls.Add(this.right1);
-            this.Controls.Add(this.bottom1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.top1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(799, 575);
+            this.Controls.Add(this.button_SAVE);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_Y_value);
+            this.Controls.Add(this.text_X_value);
+            this.Controls.Add(this.button_S);
+            this.Controls.Add(this.button_D);
+            this.Controls.Add(this.button_A);
+            this.Controls.Add(this.button_W);
+            this.Controls.Add(this.text_map);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.buttonON);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -118,12 +244,21 @@
 
         #endregion
         private System.IO.Ports.SerialPort serialPort1;
-        private System.Windows.Forms.TextBox top1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox bottom1;
-        private System.Windows.Forms.TextBox right1;
-        private System.Windows.Forms.TextBox left1;
-        private System.Windows.Forms.TextBox right2;
+        private System.Windows.Forms.Button buttonON;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.TextBox text_map;
+        private System.Windows.Forms.Button button_W;
+        private System.Windows.Forms.Button button_A;
+        private System.Windows.Forms.Button button_D;
+        private System.Windows.Forms.Button button_S;
+        private System.Windows.Forms.TextBox text_X_value;
+        private System.Windows.Forms.TextBox text_Y_value;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_SAVE;
     }
 }
 
--- Visual Studio/Form1.cs
+++ Visual Studio/Form1.cs
@@ -6,94 +6,105 @@ using System.Drawing;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
         int wartosc;
+        int x, y;   //nawigacja w tablicy, dla wygody prowadzenia pomiaru
+        int X, Y;   //podglad polozenia pudelka
 
+        int[,] mapa = new int[100, 10];
+
+        string line;
 
         public Form1()
         {
             InitializeComponent();
-            serialPort1.PortName = "COM8";
-
-            //TextBox txt1 = new TextBox();
-            top1.BackColor = Color.Red;
-
-
-
+            serialPort1.PortName = "COM17";
             serialPort1.BaudRate = 9600;
+            /*serialPort1.Parity = Parity.None;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.DataBits = 8;*/
+            x = 0;
+            y = 0;
+            X = x * 2;
+            Y = 200 - (y * 20);
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    mapa[i, j] = 0;
+                }
+            }
+
             try
             {
                 if(!serialPort1.IsOpen)
                 {
                     serialPort1.Open();
                 }
-            }
-            catch(Exception ex)
+            }catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+
             serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+
+
+            podglad();
+            text_X_value.Text += String.Format("{0,5} ", Convert.ToString(X));
+            text_Y_value.Text += String.Format("{0,5} ", Convert.ToString(Y));
         }
 
-        private void Colors()
+
+        /*ustawianie wartosci 1 lub 0 dla wykrycia obiektu lub tez nie*/
+        private void map()
         {
-            if((wartosc>=1000)&&(wartosc<2000))
+            if (wartosc < Y + 1)
             {
-                if((wartosc>=1000)&&(wartosc<1100))
-                {
-                    top1.BackColor = Color.DarkOrchid;
-                }
-                if((wartosc >= 1100) && (wartosc < 1200))
-                {
-                    top1.BackColor = Color.YellowGreen;
-                }
-
+                mapa[x,y] = 1;
             }
-            if ((wartosc >= 2000) && (wartosc < 3000))
+            else
             {
-                if ((wartosc >= 2000) && (wartosc < 2100))
-                {
-                    bottom1.BackColor = Color.DarkOrchid;
-                }
-                if ((wartosc >= 2100) && (wartosc < 2200))
-                {
-                    bottom1.BackColor = Color.YellowGreen;
-                }
-
+                mapa[x,y] = 0;
             }
-            if ((wartosc >= 3000) && (wartosc < 4000))
-            {
-                if ((wartosc >= 3000) && (wartosc < 3100))
-                {
-                    right1.BackColor = Color.DarkOrchid;
-                }
-                if ((wartosc >= 3100) && (wartosc < 3200))
-                {
-                    right1.BackColor = Color.YellowGreen;
-                }
-
-            }
-            if ((wartosc >= 4000) && (wartosc < 5000))
-            {
-                if ((wartosc >= 4000) && (wartosc < 4100))
-                {
-                    left1.BackColor = Color.DarkOrchid;
-                }
-                if ((wartosc >= 4100) && (wartosc < 4200))
-                {
-                    left1.BackColor = Color.YellowGreen;
-                }
-
-            }
-
+            text_map.ResetText();
+            podglad();
         }
 
+
+        /*podglad stanu mapy-tablicy*/
+        private void podglad()
+        {
+           /* for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    Console.Write(mapa[j,i]+" ");
+                }
+                Console.Write("\n");
+            }*/
+
+            for (int i=0; i<10; i++)
+            {
+                for(int j=0; j<100; j++)
+                {
+                    text_map.Text += String.Format("{0}", mapa[j,i]);
+                }
+                text_map.Text += "\n";
+                text_map.Text += "\n";
+            }
+        }
+
+        
+        /*odbieranie danych przez bluetooth*/
         private void DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
            // throw new NotImplementedException();
@@ -104,9 +115,6 @@ namespace WindowsFormsApplication2
                 string dane = spl.ReadLine();
                 wartosc = Convert.ToInt32(dane);
                 Console.Write("wartosc: " + wartosc + "\n");
-
-                Colors();
-
             }
             catch(Exception ex)
             {
@@ -115,9 +123,82 @@ namespace WindowsFormsApplication2
         }
 
 
-        private void button1_Click(object sender, EventArgs e)
+        /*mapowanie*/
+        private void buttonON_Click(object sender, EventArgs e)
         {
+            Console.WriteLine("button on click, x: " + x +" y: " + y);
+            map();
+            Console.WriteLine("zakonczono mapowanie");
+        }
 
+
+        /*obsluga przyciskow nawigacyjnych*/
+        private void button_W_Click(object sender, EventArgs e)
+        {
+            if (y > 0)
+            {
+                y = y - 1;
+            }
+            Console.WriteLine("y= " + y);
+            Y = 200 - (y * 20);
+            text_Y_value.Text = String.Format("{0,5} ", Convert.ToString(Y));
+        }
+
+        private void button_S_Click(object sender, EventArgs e)
+        {
+            if (y < 9)
+            {
+                y = y + 1;
+            }
+            Console.WriteLine("y= " + y);
+            Y = 200 - (y * 20);
+            text_Y_value.Text = String.Format("{0,5} ", Convert.ToString(Y));
+        }
+
+        private void button_A_Click(object sender, EventArgs e)
+        {
+            if (x > 0)
+            {
+                x = x - 1;    
+            }
+            Console.WriteLine("x= " + x);
+            X = x * 2;
+            text_X_value.Text = String.Format("{0,5} ", Convert.ToString(X));
+        }
+
+        private void button_D_Click(object sender, EventArgs e)
+        {
+            if (x < 99)
+            {
+                x = x + 1;
+            }
+            Console.WriteLine("x= " + x);
+            X = x * 2;
+            text_X_value.Text = String.Format("{0,5} ", Convert.ToString(X));
+        }
+
+
+        /*obsluga zapisu pomiarow do pliku*/
+        private void button_SAVE_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    line = Convert.ToString(mapa[j,i]);
+                    using (System.IO.StreamWriter file =
+                    new System.IO.StreamWriter(@"C:\Users\Adam\Desktop\PTM.txt", true))
+                    {
+                        file.Write(line);
+                    }
+                }
+
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"C:\Users\Adam\Desktop\PTM.txt", true))
+                {
+                    file.WriteLine("");
+                }
+            }
         }
     }
 }
